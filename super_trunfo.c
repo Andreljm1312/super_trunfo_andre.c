6b4ef0988844c5bfeb1a3319230d0eae3d54b529
Andreljm1312 <andreluis.aljm@gmail.com>
2025-04-28 13:05:46 -0300

    super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -7,14 +7,14 @@ int main(){
 //Área (float)
 //PIB (float)
 //Número de pontos turísticos (int)
-Super Trunfo{
-    char estado();//estado(ex: A;B;C)
-    char codigo[3];//codigo da carta de 01 a 04(ex: A01,B03)
-    char cidade[12];//nome da cidade(ex: Rio de Janeiro)
+struct Super Trunfo{
+    char estado;//estado(ex: A;B;C)
+    char codigo[4];//codigo da carta de 01 a 04(ex: A01,B03)
+    char cidade[20];//nome da cidade(ex: Rio de Janeiro)
     int habitantes; //numero de habitantes
     float area; //area da cidade em quilometros
     float PIB; //produto interno bruto da cidade
-    int Pontos_turisticos: //quantidade de pontos turisticos
+    int Pontos_turisticos; //quantidade de pontos turisticos
 };
 //cartas
 //cadastro da primeria carta
@@ -22,7 +22,7 @@ printf("cadastre a 1° carta:\n");
 printf("Uma letra de 'A' a 'H' (representando um dos oito estados): \n");
 scanf("%c", &carta1.estado);
 printf("digite o codigo da carta de 01 a 04(ex: A01,B03): \n");
-scanf("%s", &carta1.codigo);
+scanf("%3s", &carta1.codigo);
 printf("Digite o nome da cidade: \n");
 scanf("%s", carta1.cidade);
 printf("digite o numero de habitantes: \n");
