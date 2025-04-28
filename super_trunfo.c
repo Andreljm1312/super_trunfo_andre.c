ec492abec574c8722602af502e3fadc426607e9e
Andreljm1312 <andreluis.aljm@gmail.com>
2025-04-28 10:41:08 -0300

    super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -9,30 +9,30 @@ int main(){
 //Número de pontos turísticos (int)
 Super Trunfo{
     char estado();//estado(ex: A;B;C)
-    char codigo[](3);//codigo da carta de 01 a 04(ex: A01,B03)
-    char cidade[](12);//nome da cidade(ex: rio de janeiro)
-    int população; //numero de habitantes
+    char codigo[3];//codigo da carta de 01 a 04(ex: A01,B03)
+    char cidade[12];//nome da cidade(ex: rio de janeiro)
+    int habitantes; //numero de habitantes
     float area; //area da cidade em quilometros
     float PIB; //produto interno bruto da cidade
-    int Numeros de pontos turisticos: //quantidade de pontos turisticos
+    int Pontos_turisticos: //quantidade de pontos turisticos
 };
 //cartas
 //cadastro da primeria carta
-printf("cadastre a 1° carta:\n")
-printf("Uma letra de 'A' a 'H' (representando um dos oito estados): \n")
-scanf("%c" carta1.estado);
-printf("digite o codigo da carta de 01 a 04(ex: A01,B03): \n")
-scanf("%s", carta1.codigo);
-printf("Digite o nome da cidade: \n")
-scanf("%s"carta1.cidade);
+printf("cadastre a 1° carta:\n");
+printf("Uma letra de 'A' a 'H' (representando um dos oito estados): \n");
+scanf("%c", &carta1.estado);
+printf("digite o codigo da carta de 01 a 04(ex: A01,B03): \n");
+scanf("%s", &carta1.codigo);
+printf("Digite o nome da cidade: \n");
+scanf("%s", carta1.cidade);
 printf("digite o numero de habitantes: \n");
-scanf("%d"carta1.habitantes);
+scanf("%d", &carta1.habitantes);
 printf("Digitar a area da cidade em quilometros: \n");
-scanf("%.2f"carta1.area);
+scanf("%.2f", &carta1.area);
 printf("Digitar o PIB: \n");
-scanf("%.2f"carta1.PIB);
+scanf("%.2f", &carta1.PIB);
 prinft("Quantos pontos turisticos tem: \n");
-scanf("%d"carta1.pontosturisticos);
+scanf("%d", &carta1.Pontos_turisticos);
 
 return 0;
 }
