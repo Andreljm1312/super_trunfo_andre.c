dd6924a4c7d0e334562542cbcd7a96386614f24e
Andreljm1312 <andreluis.aljm@gmail.com>
2025-04-28 12:58:52 -0300

    super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -10,7 +10,7 @@ int main(){
 Super Trunfo{
     char estado();//estado(ex: A;B;C)
     char codigo[3];//codigo da carta de 01 a 04(ex: A01,B03)
-    char cidade[12];//nome da cidade(ex: rio de janeiro)
+    char cidade[12];//nome da cidade(ex: Rio de Janeiro)
     int habitantes; //numero de habitantes
     float area; //area da cidade em quilometros
     float PIB; //produto interno bruto da cidade
@@ -28,11 +28,22 @@ scanf("%s", carta1.cidade);
 printf("digite o numero de habitantes: \n");
 scanf("%d", &carta1.habitantes);
 printf("Digitar a area da cidade em quilometros: \n");
-scanf("%.2f", &carta1.area);
+scanf("%f", &carta1.area);
 printf("Digitar o PIB: \n");
-scanf("%.2f", &carta1.PIB);
+scanf("%f", &carta1.PIB);
 prinft("Quantos pontos turisticos tem: \n");
 scanf("%d", &carta1.Pontos_turisticos);
+
+//EXIBIÇÃO
+
+printf("Primeira carta: \n");
+printf("Estado: %c\n", carta1.estado);
+printf("Código: %s\n", carta1.codigo);
+printf("Nome da Cidade: %s\n", carta1.cidade);
+printf("População: %d\n", cartas1.habitantes);
+printf("Área: %.2f km²\n", carta1.area);
+printf("PIB:%.2f bilhões de reais\n", carta1.PIB);
+printf("Número de Pontos Turísticos: %d\n",carta1.Pontos_turisticos);
 
 return 0;
 }
