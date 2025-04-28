995d286567128dd5fe367e151701831f20c514d9
Andreljm1312 <andreluis.aljm@gmail.com>
2025-04-28 09:50:48 -0300

    super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,5 +1,30 @@
 #include <Studio.h>
 
 int main(){
-scanf 
+
+//Estrutura de dados para armazenamento
+//População (int)
+//Área (float)
+//PIB (float)
+//Número de pontos turísticos (int)
+Super Trunfo{
+    char estado();//estado(ex: A;B;C)
+    char codigo[](3);//codigo da carta de 01 a 04(ex: A01,B03)
+    char cidade[](12);//nome da cidade(ex: rio de janeiro)
+    int população; //numero de habitantes
+    float area; //area da cidade em quilometros
+    float PIB; //produto interno bruto da cidade
+    int Numeros de pontos turisticos: //quantidade de pontos turisticos
+};
+//cartas
+//cadastro da primeria carta
+printf("cadastre a 1° carta:\n")
+printf("codigo da carta de 01 a 04(ex: A01,B03)\n")
+scanf("%s", carta1° codigo);
+
+
+
+
+
+return 0;
 }
